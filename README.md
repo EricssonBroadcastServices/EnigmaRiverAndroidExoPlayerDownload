cb7408fbce0737fd69996483b9184d3dfe8535f3
empjenk <empjenk@ericsson.com>
2020-05-06 12:17:31 +0000

    r3.0.4-BETA-4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Initial commit
+# ExoPlayer Download
+
+The ExoPlayer download integration library, version r3.0.4-BETA-4.
---
+++ src/main/java/com/redbeemedia/enigma/exoplayerdownload/Placeholder.java
@@ -0,0 +1,5 @@
+package com.redbeemedia.enigma.exoplayerdownload;
+
+public class Placeholder {
+    public static final String EXOPLAYERDOWNLOAD = "EXOPLAYER"+ com.redbeemedia.enigma.download.Placeholder.DOWNLOAD;
+}
